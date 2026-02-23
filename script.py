1408844df309b9342d12e0590e46cf2fab5b9d82
Emre Guzel <136666281+PrimeEmre@users.noreply.github.com>
2026-02-23 13:26:22 -0500

    still testing but the code is finished

--- script.py
+++ script.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+from operator import index
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Setting the data
+movies = pd.read_csv("movie.csv")
+print(f"movies loaded: {movies.shape}")
+
+# Setting the volum for the movies
+movies['genres'] = movies['genres'].fillna('')
+
+# Converting gnereas to numbers
+trfidf = TfidfVectorizer(stop_words='english')
+trfidf_martix = trfidf.fit_transform(movies['genres'])
+
+# Calculating teh similarity of the movies
+similarity = cosine_similarity(trfidf_martix, trfidf_martix)
+print("Similarity matrix created!")
+# Building the recommendation
+def recommed(movie_title, num_recommendations=5):
+    indices = pd.Series(movies.index, index=movies['title']).drop_duplicates()
+
+    # Check if movie exists
+    if movie_title not in indices:
+        print(f"Movie '{movie_title}' not found")
+        return
+
+    # Get recommendations
+    indx = indices[movie_title]
+    sim_scores = list(enumerate(similarity[indx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:num_recommendations+1]
+    movie_indices = [i[0] for i in sim_scores]
+    recommendations = movies['title'].iloc[movie_indices]
+
+    print(f"\n Because you liked '{movie_title}', we recommend:")
+    for i, movie in enumerate(recommendations, 1):
+        print(f"  {i}. {movie}")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Test
+recommed("Toy Story (1995)")
+recommed("James Bond")
+recommed("GoodFellas (1990)")
+recommed("Jumanji (1995)")
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
